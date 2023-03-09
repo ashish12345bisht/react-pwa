2188af290e934b3d8950f7453ef011c670342f90
Ashish Bisht <ashish12345bisht@gmail.com>
2023-03-09 18:11:44 +0530

    screen recording pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ScreenRecording from './ScreenRecording';
 
 function App() {
   return (
     <div className="App">
-
+      <ScreenRecording />
     </div>
   );
 }
